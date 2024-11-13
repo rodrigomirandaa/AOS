19c332b012921c487e007a86d724c0c7301b000a
Rodrigo Miranda <129284231+rodrigomirandaa@users.noreply.github.com>
2024-11-12 23:02:45 -0300

    Update personalInfoRoutes.js

--- atv05/routes/personalInfoRoutes.js
+++ atv05/routes/personalInfoRoutes.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const personalInfoController = require('../controllers/personalInfoController');
 
-// Rota para obter todas as informações pessoais
 router.get('/personal-info', personalInfoController.getAllPersonalInfo);
 
-// Rota para criar uma nova informação pessoal
 router.post('/personal-info', personalInfoController.createPersonalInfo);
 
-// Rota para atualizar informações pessoais existentes
 router.put('/personal-info/:id', personalInfoController.updatePersonalInfo);
 
-// Rota para excluir informações pessoais
 router.delete('/personal-info/:id', personalInfoController.deletePersonalInfo);
 
 module.exports = router;
