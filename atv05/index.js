b042a6d09edb9c9592c446ae043f926749295d41
Rodrigo Miranda <129284231+rodrigomirandaa@users.noreply.github.com>
2024-11-12 23:03:02 -0300

    Update index.js

--- atv05/index.js
+++ atv05/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
-const experienceRoutes = require('./routes/experienceRoutes'); // Importando as rotas
+const experienceRoutes = require('./routes/experienceRoutes'); 
 const bodyParser = require('body-parser');
 const personalInfoRoutes = require('./routes/personalInfoRoutes');
 
-// Middleware para permitir o envio de JSON
 app.use(express.json());
 
-// Usando as rotas
+
 app.use('/api', experienceRoutes);
-app.use('/api', personalInfoRoutes); // Usando as rotas para a API
+app.use('/api', personalInfoRoutes);
 
 
-// Iniciando o servidor
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
